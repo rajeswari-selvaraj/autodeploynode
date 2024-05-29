2b6a5b916b612f4942335da293f127d9041f2d08
devbharath21k <devbharath21k@gmail.com>
2024-05-29 12:56:43 +0530

    new projects

---
+++ README.md
@@ -0,0 +1,14 @@
+
+![image](https://github.com/Aseemakram19/node-js-docker-cicd/assets/113539818/d179192d-90f6-4f6c-9962-818737335468)
+
+
+Follow Document link of this tutorial in the GITHUB , branch is dev
+    https://github.com/Aseemakram19/node-js-docker-cicd/tree/dev 
+    Document : Steps to follow Project 4.docx in this repo
+
+Clone the Repo below link . Branch is dev 
+   https://github.com/Aseemakram19/node-js-docker-cicd.git
+
+   Share & subscribe our channel 
+
+   https://www.youtube.com/watch?v=MFtUCfQ1RX0&t=83s 
---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Navigate to your project jenkins/workspace/ directory
+cd /var/lib/jenkins/workspace/nodeapp
+# Copy files using rsync
+cp -r * /var/nodeapp/node-js-docker-cicd
+# Navigate to your project directory
+cd /var/nodeapp/node-js-docker-cicd
+
+# Find the process ID (PID) using port 3000
+PID=$(lsof -t -i:3000)
+
+# If the PID is not empty, kill the process
+if [ -n "$PID" ]; then
+  su -c "kill -9 $PID" -s /bin/bash root
+fi
+
+# Navigate to your project directory
+cd /var/nodeapp/node-js-docker-cicd
+# stop pm2 process
+pm2 stop ecosystem.config.js
+
+# install node dependency
+npm install
+# start pm2 process
+pm2 start ecosystem.config.js
+# Find the process ID (PID) using port 3000
+PID=$(lsof -t -i:3000)
+# You may want to check if the process is still running after the deploy
+if [ -n "$PID" ]; then
+  echo "The process is still running with PID $PID."
+else
+  echo "Deployment successful.  process running on port 3000."
+fi
